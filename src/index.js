570dfeafc0e00a1380e4e4b812582371ac4300dd
Liliya <liliyavoloshine@gmail.com>
2021-11-05 10:14:47 +0400

    feat: add fullscreen layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './scss/styles.scss'
 import './js/router'
 import AudioPreloader from './js/helpers/audioPreloader'
+import VhSetter from './js/helpers/vhSetter'
 
 const audioPreloader = new AudioPreloader()
 audioPreloader.preload()
+
+const vhSetter = new VhSetter()
+vhSetter.set()
---
+++ src/js/helpers/vhSetter.js
@@ -0,0 +1,19 @@
+class VhSetter {
+  constructor() {
+    this.vh = window.innerHeight * 0.01
+    this.bind()
+  }
+
+  set() {
+    document.documentElement.style.setProperty('--vh', `${this.vh}px`)
+  }
+
+  bind() {
+    window.addEventListener('resize', () => {
+      this.vh = window.innerHeight * 0.01
+      this.set()
+    })
+  }
+}
+
+export default VhSetter
