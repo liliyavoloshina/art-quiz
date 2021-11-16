c34d8f2828260dc1731c5cfb6ce75922efebf459
Liliya <liliyavoloshine@gmail.com>
2021-11-16 13:40:17 +0400

    feat: remove some functions from critical bundle

--- src/critical.js
+++ src/critical.js
@@ -1,10 +1,2 @@
 import './scss/critical.scss'
-import AudioPreloader from './js/helpers/AudioPreloader'
-import VhSetter from './js/helpers/VhSetter'
 import './js/helpers/logging'
-
-const audioPreloader = new AudioPreloader()
-audioPreloader.preload()
-
-const vhSetter = new VhSetter()
-vhSetter.set()
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './scss/styles.scss'
 import './js/router'
+
+import AudioPreloader from './js/helpers/AudioPreloader'
+import VhSetter from './js/helpers/VhSetter'
+
+const audioPreloader = new AudioPreloader()
+audioPreloader.preload()
+
+const vhSetter = new VhSetter()
+vhSetter.set()
